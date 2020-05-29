57bb91a0ef9203f4367769670a43875d40e16c5b
git-service-user-at-920203088067 <rodrigotmuniz@gmail.com>
2020-05-29 17:08:59 -0300

    v1.2.2

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.ErrorResponseModel = require('./models/error-response-model').ErrorResponseModel;
+exports.SuccessResponseModel = require('./models/success-response-model').SuccessResponseModel;
+
 exports.isObject = require('./validations/is-object').isObject;
