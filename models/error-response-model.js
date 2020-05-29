33ff28b7db86e1db21961d8828558dc56a8f642a
git-service-user-at-920203088067 <rodrigotmuniz@gmail.com>
2020-05-29 12:35:59 -0300

    v1.1.1

--- models/error-response-model.js
+++ models/error-response-model.js
@@ -1,20 +1,10 @@
 const { isObject } = require('../verifications/is-object')
 
 exports.ErrorResponseModel = class ErrorResponseModel {
-  constructor(event, result) {
-    this._verifyInput(result);
-
-    this.event = event;
-    this.errorMessage = result.Payload.errorMessage;
-    this.code = result.Payload.code;
-    this.statusCode = result.Payload.statusCode;
-    this.executedVersion = result.ExecutedVersion;
+  constructor(inputData, errorMessage, statusCode, version) {
+    this.inputData = inputData;
+    this.errorMessage = errorMessage;
+    this.statusCode = statusCode;
+    this.version = version;
   }
-
-  _verifyInput(result) {
-    if (!isObject(result))
-      throw { input: result, errorMessage: 'result must be an object.', }
-    if (!isObject(result.Payload))
-      throw { input: result, errorMessage: 'Payload must be a object.', }
-  };
-}
+} 
--- models/error-response-model.test.js
+++ models/error-response-model.test.js
@@ -1,69 +1,20 @@
 const { ErrorResponseModel } = require('./error-response-model');
 
-describe('_verifyInput(result)', () => {
-  test('should return undefined when result is an object', async () => {
-    const expected = undefined;
-    const result = { Payload: {} }
-
-    const received = new ErrorResponseModel(null, result)._verifyInput(result);
-    expect(received).toEqual(expected);
-  });
-
-  test('should return an error object when result is not an object', async () => {
-    const notObjects = [1, 's', true, null, [], () => { }];
-
-    for (let result of notObjects) {
-      const expected = { input: result, errorMessage: 'result must be an object.' };
-      const validResult = { Payload: {} }
-
-      try {
-        new ErrorResponseModel(null, validResult)._verifyInput(result);
-      } catch (received) {
-        expect(received).toEqual(expected);
-      }
-    }
-  });
-
-  test('should return an error object when result.Payload is not an object', async () => {
-    const notObjects = [1, 's', true, null, [], () => { }];
-
-    for (let payload of notObjects) {
-      const result = {
-        Payload: payload
-      }
-      const expected = { input: result, errorMessage: 'Payload must be a object.' };
-      const validResult = { Payload: {} }
-
-      try {
-        new ErrorResponseModel(null, validResult)._verifyInput(result);
-      } catch (received) {
-        expect(received).toEqual(expected);
-      }
-    }
-  });
-})
-
-describe('constructor(event, result)', () => {
+describe('constructor(inputData, errorMessage, statusCode, version)', () => {
   test('should return a new model when request is valid', async () => {
-    const event = 'event';
-    const result = {
-      ExecutedVersion: 'executedVersion',
-      Payload: {
-        errorMessage: 'errorMessage',
-        code: 'code',
-        statusCode: 'statusCode',
-      }
-    };
+    const inputData = 'inputData';
+    const errorMessage = 'errorMessage';
+    const statusCode = 'statusCode';
+    const version = 'version';
 
     const expected = {
-      event: 'event',
+      inputData: 'inputData',
       errorMessage: 'errorMessage',
-      code: 'code',
       statusCode: 'statusCode',
-      executedVersion: 'executedVersion',
+      version: 'version',
     };
 
-    const received = new ErrorResponseModel(event, result);
+    const received = new ErrorResponseModel(inputData, errorMessage, statusCode, version);
     expect(received).toEqual(expected);
   })
 })
