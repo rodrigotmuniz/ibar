03f1614d11849e77d237038dc833d7fdda99c004
git-service-user-at-920203088067 <rodrigotmuniz@gmail.com>
2020-06-01 09:27:36 -0300

    v1.8.1

--- mocks/aws_sdk.js
+++ mocks/aws_sdk.js
@@ -16,14 +16,33 @@ const responses = {
     },
     error: {
       StatusCode: 200,
-      ExecutedVersion: 'version', 
+      ExecutedVersion: 'version',
       Payload: `{
           "inputData":{
             "method":"method",
-            "searchParams":{"TableName":"tableName"}},
+            "dbParams":{"TableName":"tableName"}},
             "errorMessage":"errorMessage",
             "statusCode":400
           }`
+    }
+  },
+  put: {
+    success: {
+      StatusCode: 200,
+      ExecutedVersion: 'version',
+      Payload: JSON.stringify({ statusCode: 200, payload: {} })
+    },
+    error: {
+      StatusCode: 200,
+      ExecutedVersion: 'version',
+      Payload: JSON.stringify({
+        inputData: {
+          method: 'put',
+          dbParams: { Item: {id: 3, name: 'Test 3'}, TableName: 'tableName' }
+        },
+        errorMessage: 'One or more parameter values were invalid: Type mismatch for key id expected: N actual: S',
+        statusCode: 400
+      })
     }
   }
 }
@@ -39,6 +58,9 @@ exports.Lambda = class Lambda {
     const isError = invokeParams.Payload.inputData != undefined;
     let method = '';
     if (invokeParams.Payload.method == 'scan') method = 'getAll';
+    else if (invokeParams.Payload.method == 'put') method = 'put';
+    else return { promise: () => Promise.reject(`${invokeParams.Payload.method} is not a valid method`) };
+
     return { promise: () => Promise.resolve(responses[method][isError ? 'error' : 'success']) };
   }
 }
@@ -65,15 +87,15 @@ const successResponse = {
 
 exports.DynamoDB = {
   DocumentClient: class DocumentClient {
-    get(searchParams) {
-      if (searchParams.error) return { promise: () => Promise.reject({ message: 'errorMessage.', statusCode: 'statusCode' }) };
-      const id = searchParams.Key.id;
+    get(dbParams) {
+      if (dbParams.error) return { promise: () => Promise.reject({ message: 'errorMessage.', statusCode: 'statusCode' }) };
+      const id = dbParams.Key.id;
       const response = successResponse.get.id[id] || {};
       return { promise: () => Promise.resolve(response) };
     }
 
-    query(searchParams) {
-      const id = searchParams.ExpressionAttributeValues[':id'];
+    query(dbParams) {
+      const id = dbParams.ExpressionAttributeValues[':id'];
       const response = successResponse.get.id[id] || { Items: [], Count: 0, ScannedCount: 0 };
       return {
         promise: () => Promise.resolve(
